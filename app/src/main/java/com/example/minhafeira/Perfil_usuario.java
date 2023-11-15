87d7eeb23d662f6477ece41866fd4f748241c48a
Paulo Carlos <paulocarlosfilho@gmail.com>
2023-11-15 14:55:53 -0300

    Perfil_Usuario

---
+++ app/src/main/java/com/example/minhafeira/Perfil_usuario.java
@@ -0,0 +1,14 @@
+package com.example.minhafeira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
