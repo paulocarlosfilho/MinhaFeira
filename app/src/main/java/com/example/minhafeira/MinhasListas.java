19507e13de33f9d40c772340c5b847ce12318120
Paulo Carlos <paulocarlosfilho@gmail.com>
2023-11-18 09:17:04 -0300

    Minhas Listas

---
+++ app/src/main/java/com/example/minhafeira/MinhasListas.java
@@ -0,0 +1,14 @@
+package com.example.minhafeira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinhasListas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minhas_listas);
+    }
+}
--- app/src/main/java/com/example/minhafeira/Perfil_usuario.java
+++ app/src/main/java/com/example/minhafeira/Perfil_usuario.java
@@ -2,13 +2,32 @@ package com.example.minhafeira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Perfil_usuario extends AppCompatActivity {
+
+    private TextView botao_minhas_listas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
+
+        iniciarComponentes();
+
+        botao_minhas_listas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Perfil_usuario.this, MinhasListas.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void iniciarComponentes(){
+        botao_minhas_listas = findViewById(R.id.button1MinhasListas);
     }
 }
