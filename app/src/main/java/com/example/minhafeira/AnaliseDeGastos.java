6763e23a9f28cabd9e71c6569a1edd1fad59c3e4
Paulo Carlos <paulocarlosfilho@gmail.com>
2023-11-18 11:49:36 -0300

    Analise de gastos - Comecei fazer

---
+++ app/src/main/java/com/example/minhafeira/AnaliseDeGastos.java
@@ -0,0 +1,14 @@
+package com.example.minhafeira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnaliseDeGastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analise_de_gastos);
+    }
+}
--- app/src/main/java/com/example/minhafeira/MinhasListas.java
+++ app/src/main/java/com/example/minhafeira/MinhasListas.java
@@ -2,13 +2,32 @@ package com.example.minhafeira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MinhasListas extends AppCompatActivity {
+
+    private TextView botao_analise_gastos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minhas_listas);
+
+        iniciarComponetes();
+
+        botao_analise_gastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MinhasListas.this, AnaliseDeGastos.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void iniciarComponetes(){
+        botao_analise_gastos = findViewById(R.id.buttonAnaliseGastos);
     }
 }
