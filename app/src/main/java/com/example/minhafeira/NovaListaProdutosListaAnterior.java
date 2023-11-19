ae92bfd97292ad16e1c895b6e2b21566bf2a2057
Paulo Carlos <paulocarlosfilho@gmail.com>
2023-11-19 08:44:55 -0300

    Criando Novas Listas - Produtos Lista Anterior - Sem backend

--- app/src/main/java/com/example/minhafeira/NovaListaProdutosListaAnterior.java
+++ app/src/main/java/com/example/minhafeira/NovaListaProdutosListaAnterior.java
@@ -2,13 +2,42 @@ package com.example.minhafeira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class NovaListaProdutosListaAnterior extends AppCompatActivity {
+
+    private TextView buttonVoltarNovaListaProdutosListaAnterior;
+    private TextView buttonSalvarNovaListaProdutosListaAnterior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_lista_produtos_lista_anterior);
+
+        iniciarComponentes();
+
+        buttonVoltarNovaListaProdutosListaAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NovaListaProdutosListaAnterior.this, CriarNovaLista.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSalvarNovaListaProdutosListaAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NovaListaProdutosListaAnterior.this, Perfil_usuario.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void iniciarComponentes(){
+        buttonVoltarNovaListaProdutosListaAnterior = findViewById(R.id.buttonVoltarNovaListaProdutosListaAnterior);
+        buttonSalvarNovaListaProdutosListaAnterior = findViewById(R.id.buttonSalvarNovaListaProdutosListaAnterior);
     }
 }
